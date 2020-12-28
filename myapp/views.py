6e069efcb76e929b85109533f90fbc2325d01b7d
Messi <messi@gmail.com>
2020-12-28 03:44:15 -0800

    Third commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
+def poster(request):
+    post=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/poster.html',{'posts':post})
 # Create your views here.
