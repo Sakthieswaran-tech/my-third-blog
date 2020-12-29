e8378c204c0ad5f99cecb6b2565e56f923e5e4a9
Messi <messi@gmail.com>
2020-12-29 05:34:20 -0800

    templates add

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.poster,name='poster.html')]
+    path('',views.poster,name='poster.html'),path('post/<int:pk>/',views.post_details,name="post_details")]
